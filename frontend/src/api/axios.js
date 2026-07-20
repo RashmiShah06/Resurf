4b8c1d08cb990645c9c0ce3c5a23c36ddaee6ce5
rashmishah346@gmail.com <rashmishah346@gmail.com>
2026-07-20 15:32:05 +0530

    updated api base url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api/v1/users",
+  baseURL: "https://resurf.onrender.com/api/v1/users",
 });
 
 export default API;
