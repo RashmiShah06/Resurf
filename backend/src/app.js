dc224aa56fcb11d8c3805e7a4133e32e58017933
rashmishah346@gmail.com <rashmishah346@gmail.com>
2026-07-20 08:28:16 +0530

    mount collection, topic, resource, trash, and search routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,32 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();//create an express app 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 //import routes
 import userRouter from './routes/user.route.js';
+import collectionRouter from "./routes/collection.route.js";
+import topicRouter from "./routes/topic.route.js";
+import resourceRouter from "./routes/resource.route.js";
+import trashRouter from "./routes/trash.route.js";
+import searchRoutes from "./routes/search.routes.js";
 
-//declare routes
+
+
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/collections", collectionRouter);
+app.use("/api/v1/topics", topicRouter);
+app.use("/api/v1/resources", resourceRouter);
+app.use("/api/v1/trash", trashRouter);
+app.use("/api/v1/search", searchRoutes);
 
-//example route : http://localhost:4000/api/v1/users/register
 
 export default app;
