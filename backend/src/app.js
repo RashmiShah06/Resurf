4e6065373cff3e98f0b5d8cb30d5ccc79a7cad9f
rashmishah346@gmail.com <rashmishah346@gmail.com>
2026-07-20 16:04:17 +0530

    allow vercel frontend origin in cors config

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ import cors from "cors";
 const app = express();//create an express app 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+     origin: ["http://localhost:5173", "https://resurf-pi.vercel.app"],
     credentials: true,
   })
 );
