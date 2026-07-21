e59b458d48b8db1247ae63e96665172773f61132
rashmishah346@gmail.com <rashmishah346@gmail.com>
2026-07-21 23:34:59 +0530

    updated mail.js

--- backend/src/utils/mail.js
+++ backend/src/utils/mail.js
@@ -3,11 +3,14 @@ import nodemailer from "nodemailer";
 export const sendEmail = async (to, subject, html) => {
 
     const transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
+        family: 4,
     });
 
     await transporter.sendMail({
